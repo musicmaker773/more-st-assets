24cd343cd0aa4c981de44e97cacd16606c29da38
Bryce Quintana <50848517+musicmaker773@users.noreply.github.com>
2025-10-25 14:26:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+"""
+# Hello World, Streamlit!
+
+This is a website to demonstrate Streamlit's API.
+You can stop looking at this now.
+
+Please.
+"""
+
+with st.form("my_form"):
+    fav_color = st.selectbox(
+        "What's your favorite color?",
+        [
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Purple"
+        ]
+    )
+    
+    reason = st.text_area("Talk about why that's your favorite color.")
+
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("It's interesting that you like " + fav_color + ".")
+        st.write("Your say it's because:")
+        st.write("""
+        ```
+        reason
+        ```
+        """.replace("reason", reason))
